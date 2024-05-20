c6fc258bfaa6e11954f91c93ee32d624b4e07777
sky <klok3113@gmaile.com>
2024-05-20 19:13:31 +0900

    javascript first class

---
+++ practice-myself/momentum/app.js
@@ -0,0 +1,28 @@
+/* js 변수 선언(const, let, var) -> var는 사용하지 말라고 함.
+const a = 10;
+const b = 2;
+let myName = "kiho";
+
+console.log(a * b);
+console.log(a + b);
+console.log(a - b);
+console.log("hello " + myName);
+
+myName = "legolas";
+
+console.log("your new name is " + myName);*/
+
+
+
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+console.log(daysOfWeek[3]);
