6b4f9df839cbaaab62289cf1035c835b353e93bb
sky <klok3113@gmaile.com>
2024-05-22 20:18:18 +0900

    javascript function_notes

--- practice-myself/momentum/app.js
+++ practice-myself/momentum/app.js
@@ -25,3 +25,34 @@ const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
 console.log(daysOfWeek[3]);*/
 
+// const player = {
+//   name: "kiho",
+//   points: 10,
+//   fat: true,
+// };
+// console.log(player);
+// player.lastName = "potato";
+// player.points = player.points + 10;
+// console.log(player);
+
+// function
+
+    function plus(a, b) {
+        console.log(a + b);
+    }
+    function divide(a, b){
+        console.log(a / b);
+    }
+    plus(8, 60);
+    divide(60, 10);
+
+
+    const player = {
+        name : "Jym Moriaty",
+        sayHello : function (otherPerson){
+            console.log("Hello! My name is " + otherPerson + ". Are you miss me?")
+        },
+    };
+    console.log(player.name);
+    player.sayHello("Jym Moriaty")
+    
